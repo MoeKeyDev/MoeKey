e24d6c88f188fff24ebbd6ec2a53b0d91926a612
ChenDoXiu <chendoxiu@outlook.com>
2024-01-31 17:46:06 +0800

    Add IOS,macOS Support update App Id

--- android/app/src/main/kotlin/com/doxiu/misskey/misskey/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package love.moegirl.misskey.misskey
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/love/moegirl/moekey/MainActivity.kt
@@ -0,0 +1,6 @@
+package love.moegirl.moekey
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
