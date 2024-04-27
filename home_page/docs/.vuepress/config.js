136cf7307005b358da0c59be38cface3c3419232
noDream <1802024110@qq.com>
2024-04-27 12:54:07 +0800

    添加了MoeThe configuration file and README document of the Key project

---
+++ home_page/docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'MoeKey',
+    description: 'A Misskey client for Flutter',
+    head: [
+        ['link', { rel: 'icon', href: 'https://jsdelivr.007666.xyz/gh/ChenDoXiu/MoeKey@main/assets/favicon.ico' }]
+    ],
+    themeConfig: {
+        navbar: false,
+        search: false
+    }
+}
